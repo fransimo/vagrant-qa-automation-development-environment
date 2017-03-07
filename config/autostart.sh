69ae7a911109fa8ef5509695389b13006906fdb9
Francisco Simo Maimo <fsimo@hotelbeds.com>
2017-03-07 11:06:36 +0100

    auto update

--- config/autostart.sh
+++ config/autostart.sh
@@ -2,11 +2,50 @@
 
 setxkbmap es
 
-if [ $? -eq 0 ] && [ -f ~/firstRun.sh ] ; then
-	#run only in X session and only once
-	mv firstRun.sh firstRun_running.sh
-	bash firstRun_running.sh
-	mv firstRun_running.sh firstRun_runned.sh
+if [ $DISPLAY == ":0" ] ; then
+	#run only in X session
+	vibase=$HOME/vagrant-image-update
+	
+	#check update structure
+	if [ ! -d "$vibase" ] ; then
+		mkdir "$vibase"
+	fi
+	
+	#download updates repos
+	if [ ! -d "$vibase/vagrant-qa-automation-development-environment" ] ; then
+		cd $vibase
+		git clone https://github.com/fransimo/vagrant-qa-automation-development-environment.git
+	else
+		repo="$vibase/vagrant-qa-automation-development-environment"
+		if [ $(stat -c %Y $repo) -lt $(date +%s --date="120 min ago") ] ; then 
+			echo update repo, more than 120 mins
+			cd $repo
+			git pull
+			touch $repo
+		fi 
+	fi
+	
+	if [ -f "$vibase/vagrant-qa-automation-development-environment/imageupdate.sh" ] ; then
+		bash $vibase/vagrant-qa-automation-development-environment/imageupdate.sh
+	fi
+	
+	#download local updates repos
+	#if [ ! -d "$vibase/localrepo" ] ; then
+	#	cd $vibase
+	#	git clone localrepo
+	#else
+	#	repo="$vibase/localrepo"
+	#	if [ $(stat -c %Y $repo) -lt $(date +%s --date="15 min ago") ] ; then 
+	#		echo update repo, more than 15 minute
+	#		cd $repo
+	#		git pull
+	#		touch $repo
+	#	fi 	fi
+	#
+	#if [ -f "$vibase/vagrant-qa-hbg/imageupdate.sh" ] ; then
+	#	bash $vibase/vagrant-qa-hbg/imageupdate.sh
+	#fi
+	
 fi
 
 
