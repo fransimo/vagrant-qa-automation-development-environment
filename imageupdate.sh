e99496c7dc4e4573799cd51747df49e96b128bc3
Francisco Simo Maimo <fsimo@hotelbeds.com>
2017-03-09 16:08:38 +0100

    error control for update

--- imageupdate.sh
+++ imageupdate.sh
@@ -7,6 +7,8 @@ for f in $(find $SCRIPTPATH/updates -type d | sort ) ; do
 	if [ -f $f/update_script.sh ] && [ ! -f $f/runned.txt ] ; then
 		echo Running $f/update_script.sh
 		bash $f/update_script.sh
-		touch $f/runned.txt
+		if [ $? -eq 0 ] ; then
+                        touch $f/runned.txt
+                fi
 	fi
 done
