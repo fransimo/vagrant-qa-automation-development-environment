e535f4941a6429f6d755b2ccc65fe87b492ff36d
Francisco Simo Maimo <fsimo@hotelbeds.com>
2017-03-16 18:12:27 +0100

    jmeter 3.1

--- updates/2017-03-16/update_script.sh
+++ updates/2017-03-16/update_script.sh
@@ -12,6 +12,7 @@ if [ $ret -eq 0 ] ;  then
 	rm apache-jmeter-3.1.tgz*
 	wget http://apache.rediris.es/jmeter/binaries/apache-jmeter-3.1.tgz
 	sudo tar xf apache-jmeter-3.1.tgz -C /opt/
+
 	if [ $? -eq 0 ] ; then
 		rm apache-jmeter-3.1.tgz
 		
@@ -23,9 +24,12 @@ if [ $ret -eq 0 ] ;  then
 		#gnome settings
 		gsettings set com.canonical.Unity.Launcher favorites "['application://ubiquity.desktop', 'application://org.gnome.Nautilus.desktop', 'application://firefox.desktop', 'application://chromium-browser.desktop', 'application://jetbrains-idea-ce.desktop', 'application://eclipse.desktop', 'application://syntevo-smartgit.desktop', 'application://gnome-terminal.desktop', 'application://selenium-start.desktop','application://sqldeveloper.desktop', 'application://jmeter.desktop', 'application://unity-control-center.desktop', 'unity://running-apps', 'unity://expo-icon', 'unity://devices']" 
 	else
+		echo "Error downloading jmeter 3.1"
 		ret=1
 	fi
 
+else
+	echo "Will be asked next session"
 fi
 
 exit $ret
