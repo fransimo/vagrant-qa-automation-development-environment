d7ef9e87760f44696bb87487297b9b09935c4c27
Francisco Simo Maimo <fsimo@hotelbeds.com>
2017-03-16 17:56:39 +0100

    jmeter 3.1

---
+++ updates/2017-03-16/update_script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+
+
+zenity --question --text "Do you want to install jmeter 3.1?"
+ret=$?
+
+if [ $ret -eq 0 ] ;  then
+	#jmeter
+	wget --quiet http://apache.rediris.es/jmeter/binaries/apache-jmeter-3.1.tgz
+	sudo tar xf apache-jmeter-3.1.tgz -C /opt/
+	if [ $? -eq 0 ] ; then
+		rm apache-jmeter-3.1.tgz
+		
+		sudo apt-get -y remove jmeter jmeter-http
+
+		cp $SCRIPTPATH/jmeter.desktop /home/vagrant/.config/apps
+		cp $SCRIPTPATH/jmeter.desktop /home/vagrant//.local/share/applications
+
+		#gnome settings
+		gsettings set com.canonical.Unity.Launcher favorites "['application://ubiquity.desktop', 'application://org.gnome.Nautilus.desktop', 'application://firefox.desktop', 'application://chromium-browser.desktop', 'application://jetbrains-idea-ce.desktop', 'application://eclipse.desktop', 'application://syntevo-smartgit.desktop', 'application://gnome-terminal.desktop', 'application://selenium-start.desktop','application://sqldeveloper.desktop', 'application://jmeter.desktop', 'application://unity-control-center.desktop', 'unity://running-apps', 'unity://expo-icon', 'unity://devices']" 
+	else
+		ret=1
+	fi
+
+fi
+
+exit $ret
+
+
